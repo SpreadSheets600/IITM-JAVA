fa809d5886f3358cf15b81702f7b1b691118f37e
SpreadSheets600 <sohammaity006@gmail.com>
2025-05-11 22:43:12 +0530

    feat: first java program

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This Is My First Program Written In Java");
+    }
+}
